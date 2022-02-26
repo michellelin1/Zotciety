58419e26a193a821f77b9a318edc9c1e6babad06
chloecheng8 <30807247+chloecheng8@users.noreply.github.com>
2022-02-26 10:59:43 -0800

    updated activity cards

--- src/pages/prompts/Prompts.jsx
+++ src/pages/prompts/Prompts.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import ActivityCard from "../../components/activityCard/ActivityCard"
 
 export function Prompts() {
   return (
     <div>
       <h1>prompts page</h1>
+      <ActivityCard />
     </div>
   );
 }
