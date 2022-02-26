fc0b42be7b032562b984a746b657023914db1869
chloecheng8 <30807247+chloecheng8@users.noreply.github.com>
2022-02-26 12:30:28 -0800

    updated activity card

--- src/components/activityCard/ActivityCard.jsx
+++ src/components/activityCard/ActivityCard.jsx
@@ -3,7 +3,7 @@ import "./ActivityCard.css"
 
 export default function ActivityCard() {
   let prompt = "Prompt"
-  let description = "description"
+  let description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed fringilla erat, nec commodo metus. Nunc eleifend rhoncus rutrum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer vitae orci at massa interdum convallis. Proin malesuada mauris tellus, non congue massa eleifend iaculis. Vestibulum quis velit augue. Vestibulum odio nulla, finibus non sodales ut, laoreet et tortor. Nam non nibh nec erat placerat porttitor in vitae nisi. Pellentesque et mattis diam, ut consectetur quam. fdfsfafewd, consectetur adipiscing elit. Aliquam sed fringilla erat, nec commodo metus. Nunc eleifend rhoncus rutrum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer vitae orci at massa interdum convallis. Proin malesuada mauris tellus, non congue massa eleifend iaculis. Vestibulum quis velit augue. Vestibulum odio nulla, finibus non sodales ut, laoreet et tortor. Nam non nibh nec erat placerat porttitor in vitae nisi. Pellentesque et mattis diam, ut consectetur quam."
   let point = "points"
 
   function CompletedButton() {
@@ -13,19 +13,21 @@ export default function ActivityCard() {
     }
 
     return (
-      <button class="btn" onClick={click}>Completed</button>
+      <button class="clickBtn" onClick={click}>Completed</button>
     )
   }
 
   return (
-    <div class="card">
-        <div class="first">
+    <div class="card container">
+      <div class="row">
+        <div class="first col">
     			<h1><strong>{prompt}</strong></h1>
           <p>{description}</p>
         </div>
-        <div class="second">
+        <div class="second col">
           <p>{point}</p>
           <div class="button"><CompletedButton /></div>
+        </div>
         </div>
     </div>
     );
