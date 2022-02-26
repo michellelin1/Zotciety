10c8c009663ab2ff98b61d1de173f051e48e6ac0
ThatMegamind <udongenius@hotmail.com>
2022-02-26 09:54:27 -0800

    Home Page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import './Home.css';
+
 
 export function Home() {
   return (
     <div>
-			<h1>home page</h1>
+			<h1 className = "TitleFont titleposition" >Zotciety</h1>
+      <div className = "Container">
+        <div className = "row position">
+          <div className = "col">
+            <button className = "buttons ButtonFont">ZotPrompts</button>
+          </div>
+          <div className = "col">
+            <button className = "buttons ButtonFont">ZotTrials</button>
+          </div>
+        </div>
+      </div>
     </div>
     );
 }
