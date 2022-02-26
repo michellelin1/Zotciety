a87e6548123720b84a41ef996b7f70a78ffb78ba
ThatMegamind <udongenius@hotmail.com>
2022-02-26 10:49:01 -0800

    Home updates #2

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,13 +3,23 @@ import './Home.css';
 
 
 export function Home() {
+  function PromptButton() {
+    function handleClick() {
+      window.open("https://www.google.com");
+    }
+    return (
+      <button className = "buttons ButtonFont" onClick={handleClick}>
+        ZotPrompts
+      </button>
+    );
+  }
   return (
     <div>
 			<h1 className = "TitleFont titleposition" >Zotciety</h1>
-      <div className = "Container">
+      <div className = "Container backgroundimage">
         <div className = "row position">
           <div className = "col">
-            <button className = "buttons ButtonFont">ZotPrompts</button>
+            <PromptButton />
           </div>
           <div className = "col">
             <button className = "buttons ButtonFont">ZotTrials</button>
