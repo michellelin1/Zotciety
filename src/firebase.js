4c71497184779900ce0646dc90521d3c44da1f2d
ThatMegamind <udongenius@hotmail.com>
2022-02-26 16:38:45 -0800

    Firebase1

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore";
+import { collection, getDocs, addDoc } from "firebase/firestore";
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyC_2nNn0MniV38sMAezT7DX5NmVfeJjyHs",
+    authDomain: "zotciety.firebaseapp.com",
+    projectId: "zotciety",
+    storageBucket: "zotciety.appspot.com",
+    messagingSenderId: "825597162128",
+    appId: "1:825597162128:web:ddfaebc62fdab6541cde15"
+  });
+
+const db = getFirestore();
